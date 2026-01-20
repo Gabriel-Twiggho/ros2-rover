79d620a4394ad028ac00b01d2be896cd45163be8
Gabriel-Twiggho <104115173+Gabriel-Twiggho@users.noreply.github.com>
2026-01-20 18:48:44 +1100

    Delete scripts directory

--- scripts/run_audio_capture.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-cd ~/ros2_iron
-source ~/ros2_venv/bin/activate
-source install/setup.bash
-python3 src/audio_capture_node/audio_capture_node/audio_capture_py.py
